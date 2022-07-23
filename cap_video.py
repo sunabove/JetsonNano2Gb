28f322fc3ab773a75688044142395da5807f2070
sunabove <sunabove@nate.com>
2022-07-23 17:20:02 +0900

    **

--- cap_video.py
+++ cap_video.py
@@ -4,7 +4,33 @@ from time import sleep
 
 print( "Hello ..." )
 
-cap = cv.VideoCapture(0)
+def gstreamer_pipeline(
+        camera_id,
+        capture_width=1920,
+        capture_height=1080,
+        display_width=1920,
+        display_height=1080,
+        framerate=30,
+        flip_method=0,
+    ):
+    return (
+            "nvarguscamerasrc sensor-id=%d ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=True"
+            % ( camera_id, capture_width, capture_height,
+                    framerate, flip_method,
+                    display_width, display_height,
+            )
+    )
+   
+cap = cv.VideoCapture( gstreamer_pipeline(camera_id=0, display_width=640, display_height=480), cv.CAP_GSTREAMER)
+
+#cap = cv.VideoCapture(0)
 
 for i in range( 100 ) :
     # Capture frame-by-frame
@@ -12,7 +38,7 @@ for i in range( 100 ) :
 
     # Display the resulting frame
     cv.imshow('frame', frame)
-    sleep( 0.1 )
+    sleep( 0.01 )
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 

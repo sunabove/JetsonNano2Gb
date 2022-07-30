92d008b641bb6cdfc38563a8f910327ece969229
sunabove <sunabove@nate.com>
2022-07-30 16:45:56 +0900

    **

--- battery_volt.py
+++ battery_volt.py
@@ -1,5 +1,8 @@
-import board, busio
+import os, board, busio
 import adafruit_ina219
+
+address = os.popen("i2cdetect -y -r 1 0x42 0x42 | egrep '42' | awk '{print $2}'").read().strip() 
+print( f"address = {address}")
 
 i2c = busio.I2C(board.SCL, board.SDA)
 ina219 = adafruit_ina219.INA219(i2c)

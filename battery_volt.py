79979036fb3e46844dfcfa0d555f881a81b71603
sunabove <sunabove@nate.com>
2022-07-30 16:42:29 +0900

    **

--- battery_volt.py
+++ battery_volt.py
@@ -4,6 +4,6 @@ import adafruit_ina219
 i2c = busio.I2C(board.SCL, board.SDA)
 ina219 = adafruit_ina219.INA219(i2c)
 
-print( f"Bus Voltage:   {ina219.bus_voltage :.3f } V" )
-print( f"Shunt Voltage: {ina219.shunt_voltage / 1000 :.3f } mV" )
-print( f"Current:       {ina219.current:.3f } mA" )
+print( f"Bus Voltage:   {ina219.bus_voltage} V" )
+print( f"Shunt Voltage: {ina219.shunt_voltage / 1000 } mV" )
+print( f"Current:       {ina219.current} mA" )

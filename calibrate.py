ba8cdf90e0b544757d48ad1796a4c42dba1539be
sunabove <sunabove@nate.com>
2022-08-01 21:27:08 +0900

    **

--- calibrate.py
+++ calibrate.py
@@ -2,7 +2,7 @@ import numpy as np, cv2 as cv
 import glob
 
 # termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
@@ -18,7 +18,7 @@ def gstream_pipeline(
         camera_id=0, width=1920, height=1080, framerate=10, flip_method=0 ):
     return f"nvarguscamerasrc sensor-id={camera_id} ! video/x-raw(memory:NVMM), width=(int){width}, height=(int){height}, format=(string)NV12, framerate=(fraction){framerate}/1 ! nvvidconv flip-method={flip_method} ! video/x-raw, width=(int){width}, height=(int){height}, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=True"  
 
-size_factor = 4
+size_factor = 2
 GSTREAMER_PIPELINE = gstream_pipeline(width=1280//size_factor, height=960//size_factor) 
 cap = cv.VideoCapture(GSTREAMER_PIPELINE, cv.CAP_GSTREAMER)
 
@@ -34,7 +34,7 @@ while cap.isOpened() :
     ret, corners = cv.findChessboardCorners(gray, (7,6),None)
 
     # If found, add object points, image points (after refining them)
-    if ret == True:
+    if ret :
         objpoints.append(objp)
 
         corners2 = cv.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
@@ -42,8 +42,14 @@ while cap.isOpened() :
 
         # Draw and display the corners
         img = cv.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv.imshow('img',img)
-        cv.waitKey(500)
+        cv.imshow('img', img) 
+    else :
+        cv.imshow('img', gray) 
     pass
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    pass
+pass
 
 cv.destroyAllWindows()

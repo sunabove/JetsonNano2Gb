b542cf2b258f996cca1cbcb80ebed0633c3d01af
sunabove <sunabove@nate.com>
2022-08-09 17:48:07 +0900

    **

--- motor_cali.py
+++ motor_cali.py
@@ -12,8 +12,23 @@ print("Done initializing servokit.")
 
 motor = kit.continuous_servo[ 1 ]
 
+def set_throttle( throttle_to) : 
+    diff = throttle_to - motor.throttle 
+    while abs( diff ) > 0.01 :
+        inc = 0.01*np.sign( diff )
+        print( f"curr throttle = {motor.throttle:.4f}, to throttle = {throttle_to}, inc = {inc:.4f}" )
+        motor.throttle += inc
+        sleep( 0.1 )
+        diff = throttle_to - motor.throttle 
+    pass
+
+    motor.throttle = throttle_to
+    diff = throttle_to - motor.throttle 
+    print( f"curr throttle = {motor.throttle:.4f}, to throttle = {throttle_to}, inc = {inc:.4f}" )
+pass
+
 print( "Sending maximum throttle signal ...." )
-motor.throttle = 1.0
+set_throttle( 1.0 )
 sleep( 1 )
 
 print( "Connect the Motor cable to the power! " )
@@ -22,7 +37,7 @@ print( "Wait for 5 seconds! " )
 sleep( 5 )
 
 print( "Sending minimum throttle signal ...." )
-motor.throttle = 0.0
+set_throttle( 0.0 )
 sleep( 5 )
 
 print( "Good bye!" )

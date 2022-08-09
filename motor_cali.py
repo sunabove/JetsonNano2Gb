bd73e2345b027fa11daa4462c59bf018abce5385
sunabove <sunabove@nate.com>
2022-08-09 11:22:05 +0900

    **

--- motor_cali.py
+++ motor_cali.py
@@ -2,8 +2,8 @@ from adafruit_servokit import ServoKit
 import board, busio, numpy as np
 from time import sleep
 
-text = input ( "Hello" )
-print( text )
+print( "Disconnect the Motor cabel from the power!" )
+text = input ( "Enter to continue! " )
 
 print("Initializing ServoKit ...")
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -12,24 +12,16 @@ print("Done initializing servokit.")
 
 motor = kit.continuous_servo[ 1 ]
 
-min_throttle = -0.7
-max_throttle = 0.3 
-
-motor.throttle = -1.0
+print( "" )
+motor.throttle = 1.0
 sleep( 1 )
 
-duration = 0.2
-for throttle in np.arange( min_throttle, max_throttle, 0.01 ) :
-    print( f"throttle = {throttle:.2f}", flush=True )
-    motor.throttle = throttle
-    sleep( duration )
+print( "Connect the Motor cable to the power! " )
+input( "Enter to continue ..." )
+print( "Wait for 5 seconds! " )
+sleep( 5 )
 
-for throttle in np.arange( max_throttle, min_throttle, -0.01 ) :
-    print( f"throttle = {throttle:.2f}", flush=True )
-    motor.throttle = throttle
-    sleep( duration ) 
-
-motor.throttle = -1.0
+motor.throttle = 0
 sleep( 1 )
 
 print( "Good bye!" )

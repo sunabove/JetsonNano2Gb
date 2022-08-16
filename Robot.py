aae748625ff66faaf8f9a2ceed863ae1efe9d887
sunabove <sunabove@nate.com>
2022-08-16 14:26:12 +0900

    **

--- Robot.py
+++ Robot.py
@@ -1,213 +1,116 @@
 #coding: utf-8
 
 import sys, numpy as np, threading, logging as log, inspect, signal
-import RPi.GPIO as GPIO
-
-from Config import cfg
+import cv2 as cv, psutil
 
 from time import time, sleep
-from gpiozero import Buzzer
-from Motor import Motor
-from RGB_LED import RGB_LED
-from Camera import Camera, generate_frame
-from Servo import Servo
-from IRRemote import IRRemote
-from rpi_ws281x import Color
-from JoyStick import JoyStick
-from ObstacleSensor import ObstacleSensor
-from LineTrackerPID import LineTrackerPID
-from LineCamera import LineCamera
-import Functions as funtions
+from flask import Flask, render_template, Response, request, jsonify
 
 log.basicConfig(
     format='%(asctime)s, %(levelname)-8s [%(filename)s:%(lineno)04d] %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S', level=log.INFO
     )
 
-class Robot :
-    def __init__(self): 
-        super().__init__()
+app = None
 
-        self.service = None
-        self.beep_time = [0]*4 # 전후좌우
+def gstream_pipeline(
+        camera_id=0, width=1920, height=1080, framerate=10, flip_method=0 ):
+    return f"nvarguscamerasrc sensor-id={camera_id} ! video/x-raw(memory:NVMM), width=(int){width}, height=(int){height}, format=(string)NV12, framerate=(fraction){framerate}/1 ! nvvidconv flip-method={flip_method} ! video/x-raw, width=(int){width}, height=(int){height}, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=True"  
 
-        self.buzzer     = Buzzer(4)   # 부저
-        self.motor      = Motor()
-        self.rgb_led    = RGB_LED()
-        self.camera     = Camera( motor = self.motor )
-        self.servo      = Servo()
-        self.joyStick   = JoyStick( self.servo, buzzer=self.buzzer )
-        self.irremote   = IRRemote( self, buzzer=self.buzzer )
+size_factor = 2
+GSTREAMER_PIPELINE = gstream_pipeline(width=1280//size_factor, height=960//size_factor) 
+camera = cv.VideoCapture(GSTREAMER_PIPELINE, cv.CAP_GSTREAMER)
 
-        overlay = "grayscale" # "successive"
+video_frame = None
+frame_no = 0 
+capture_thread = None
 
-        self.config = { 'min_speed' : self.motor.min_speed , 'threshold': cfg( 'threshold', 85 ), 'overlay' : overlay, 'move' : False }  #65
+def putTextLine(image, txt, x, y, fg_color=None, bg_color=None, font_size=0.4 ) :
+    # opencv 이미지에 텍스트를 그린다.
+    font = cv.FONT_HERSHEY_SIMPLEX
+    fs = font_size  # font size(scale)
+    ft = 1    # font thickness 
 
-        # 시동 소리 내기
-        self.rgb_led.light_effect( "flash", Color(0, 255, 0), duration=3 )         
-        self.buzzer.beep(on_time=0.1, off_time=0.1, n = 4)
+    if fg_color is None : 
+        fg_color = (255, 0, 0) # text foreground color
+    
+    if bg_color is None :
+        bg_color = (255, 255, 255) # text background color
 
-        # joystick service start
-        threading.Thread(target=self.joyStick.service).start()
+    if image is not None and len( image.shape ) == 2 : #gray scale
+        bg_color = (255, 255, 255) # white
+        fg_color = (0, 0, 0)  # black
     pass
 
-    def __del__(self):
-        self.finish()
+    cv.putText(image, txt, (x, y), font, fs, bg_color, ft + 2, cv.LINE_AA)
+    cv.putText(image, txt, (x, y), font, fs, fg_color, ft    , cv.LINE_AA) 
+pass # -- putTextLine
+
+frame_cnt = 0 
+def process_image( image ) :
+    global frame_cnt, frame_no
+
+    text = f"FRM NO: {frame_cnt}/{frame_no}, Ratio = { (frame_cnt/frame_no)*100:3.1f} %" 
+    tx = 10
+    ty = 20
+    th = 20   # line height
+    fg_color = (0, 255, 0) 
+    bg_color = (50, 50, 60)
+    putTextLine( image, text, tx, ty, fg_color, bg_color )
+
+    text = ""
+    pct = psutil.virtual_memory()[2]  # RAM 사용량 출력 
+    text += f"MEM : {pct:02.1f} %"
+    
+    pct = psutil.cpu_percent() # CPU 사용량 출력 
+    text += f" CPU : {pct:03.1f} %"
+
+    ty += th
+    fg_color = (0, 0, 255) if pct >= 90 else (0, 255, 0)
+    bg_color = (50, 50, 60)
+    putTextLine( image, text, tx, ty, fg_color, bg_color )
+
+    frame_cnt += 1
+    return image
+pass
+
+def capture_frames():
+    global video_frame, frame_no
+
+    while camera.isOpened():
+        ret, frame = camera.read()
+        if not ret :
+            break
+
+        video_frame = frame
+        frame_no += 1
     pass
+pass
 
-    def finish(self) :
-        log.info(inspect.currentframe().f_code.co_name) 
+def encode_frame():
+    global video_frame, frame_no
+    curr_frame_no = frame_no -1
 
-        self.stop_service()
+    while True:
+        # Acquire thread_lock to access the global video_frame object
+        if ( video_frame is not None ) and ( curr_frame_no < frame_no ):
+            curr_frame_no = frame_no
+
+            frame = process_image( video_frame )
+            ret, encoded_image = cv.imencode(".jpg", frame)
         
-        self.buzzer.close()
-        log.info( "buzzer finished" ) 
+            if not ret:
+                continue
+            pass 
 
-        self.motor.finish()
-        log.info( "motor finished" ) 
-
-        self.rgb_led.finish() 
-        log.info( "rgg_led finished" ) 
-
-        self.camera.finish()
-        log.info( "camera finished" ) 
-
-        self.servo.finish()
-        log.info( "servo finished" ) 
-
-        self.irremote.finish()
-        log.info( "irremote finished" ) 
-
-        self.joyStick.finish()
-        log.info( "joyStick finished" )
-    pass
-
-    def speed_left(self) :
-        return self.motor.speed_left()
-    pass
-
-    def speed_right(self) :
-        return self.motor.speed_right()
-    pass
-
-    def speed_min(self) :
-        return self.motor.speed_min()
-    pass
-
-    def stop_service(self) :
-        service = self.service 
-        self.service = None
-
-        if service :
-            service.stop()
+            # Output image as a byte array
+            yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+                bytearray(encoded_image) + b'\r\n')
+        else : 
+            sleep( 0.1 )
         pass
-    pass # -- stop_service
-
-    def stop(self) :
-        self.stop_robot()
     pass
-
-    def stop_robot(self) :
-        log.info(inspect.currentframe().f_code.co_name) 
-
-        self.motor.stop_motor()
-        self.rgb_led.turn_off() # RGB LED 꺼기
-    pass
-
-    def speed_up( self, dv = 5 ) : # 속도 증가
-        self.motor.speed_up( dv )
-    pass
-
-    def speed_down( self, dv = 5) : # 속도 감소 
-        self.motor.speed_down( dv )
-    pass
-
-    def forward(self, speed = None) :
-        # 전진
-        # 전진 경고음
-        now = time()
-        if now - self.beep_time[0] > 3 : # 최소 5초후에 소리를 낸다.
-            self.beep_time[0] = now 
-            self.buzzer.beep(on_time=0.5, off_time=0.5, n = 1)
-
-            # 전진 라이트 
-            self.rgb_led.light_effect( "breath", Color(0, 255, 0) )
-        pass        
-        
-        self.motor.forward( speed )
-    pass
-
-    def move(self, left = None, right=None):
-        self.motor.move( left, right )
-    pass # -- move
-
-    def backward(self, speed = None) :
-        # 후진 
-        # 후진시 경고음 
-        now = time()
-        if now - self.beep_time[1] > 3 : # 최수 3초후에 소리를 낻다.
-            self.beep_time[1] = now
-            self.buzzer.beep(on_time=0.05, off_time=0.05, n = 3)
-            
-            # 후진시에는 빨간색으로 깜박인다.
-            self.rgb_led.light_effect( "flash", Color(255, 0, 0) ) 
-        pass
-
-        self.motor.backward( speed )
-    pass
-
-    def left(self, turn_speed=None) : # 좌회전
-        # 경고음 
-        now = time()
-        if now - self.beep_time[2] > 3 : # 최수 3초후에 소리를 낻다.
-            self.beep_time[2] = now 
-            self.buzzer.beep(on_time=0.1, off_time=0.1, n = 2)
-        pass
-
-        self.motor.left(turn_speed)
-        # LED 깜빡이기
-    pass
-
-    def right(self, turn_speed=None): # 우회전 
-        # 경고음 
-        now = time()
-        if now - self.beep_time[3] > 3 : # 최수 3초후에 소리를 낻다.
-            self.beep_time[3] = now
-            self.buzzer.beep(on_time=0.1, off_time=0.1, n = 2)
-        pass
-
-        self.motor.right()
-    pass
-
-    # servo control
-    def servo_left( self ) : 
-        self.servo.left()
-    pass
-
-    def servo_right( self ) : 
-        self.servo.right()
-    pass
-
-    def servo_up( self ) : 
-        self.servo.up()
-    pass
-
-    def servo_down( self ) : 
-        self.servo.down()
-    pass
-
-    def servo_stop( self ) : 
-        self.servo.stop()
-    pass
-
-    def stop_servo( self ) : 
-        self.servo.stop()
-    pass
-
-pass # -- Robot
-
-app = None 
-robot = None 
+pass
 
 def stop():
     global robot, app
@@ -216,39 +119,29 @@ def stop():
 
     log.info( 'Robot stopping ...' ) 
 
-    buzzer = robot.buzzer if robot else Buzzer( 4 ) 
-
-    buzzer.beep(on_time=1, off_time=1, n = 1, background=False)
-
-    if robot :    
-        robot.finish()
-    pass
-
-    if app : 
+    if app is not None : 
         app.do_teardown_appcontext()
     pass
 
     log.info( "Good bye!" )
 pass # -- stop
 
-def service() : 
-    global app, robot
+def start() : 
+    global app, capture_thread
 
     log.info( "Hello....." )
 
     # web by flask framewwork
-    from flask import Flask, render_template, Response, request, jsonify
-
-    GPIO.setwarnings(False)
 
     app = Flask(__name__, static_url_path='', static_folder='html', template_folder='html')
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     
-    robot = Robot()
-    robot.camera.start_recording()    
-
     @app.before_request
     def before_request_func():
+        if capture_thread is None : 
+            capture_thread = threading.Thread(target=capture_frames)
+            capture_thread.daemon = True
+            capture_thread.start()
         pass
     pass
 
@@ -256,14 +149,14 @@ def service() :
     @app.route( '/index.html' )
     @app.route( '/index.htm' )
     def index(): 
-        config = robot.config
+        config = {}
         
         return render_template('index_robot_server.html', **config )
     pass 
 
-    @app.route('/video_feed')
+    @app.route('/video')
     def video_feed(): 
-        return Response(generate_frame(robot.camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(encode_frame(), mimetype = "multipart/x-mixed-replace; boundary=frame") 
     pass 
 
     @app.route("/cmd", methods=['POST'] )
@@ -299,23 +192,6 @@ def service() :
             robot.servo_up()
         elif cmd == "servo_down":
             robot.servo_down()
-        elif cmd == "shutdown" :
-            funtions.shutdown(robot.buzzer)
-        elif cmd == "obstacle_sensor" :
-            robot.stop_service()
-            robot.service = ObstacleSensor( robot=robot )
-            robot.service.start()
-        elif cmd == "line_tracking" :
-            robot.stop_service()
-            robot.service = LineTrackerPID( robot=robot, buzzer = robot.buzzer )
-            robot.service.start()
-        elif cmd == "line_camera" :
-            if robot.service is None or type( robot.service ) != LineCamera :
-                robot.stop_service()
-                config[ "move" ] = False
-                robot.service = LineCamera( robot=robot, camera=robot.camera, buzzer = robot.buzzer )
-                robot.service.start()
-            pass
         elif cmd == "move" :
             config[ "move" ] = not config[ "move" ] 
 
@@ -326,17 +202,6 @@ def service() :
             pass
             
             log.info( f"move = {config['move']}")
-        elif cmd == "min_speed" :
-            if val :
-                robot.motor.min_speed = min( 70, max( 5, int(val) ) )
-            pass
-        elif cmd == "threshold" :
-            if val :
-                cfg( "threshold", int(val), save=True )
-                robot.config[ "threshold" ] = cfg( "threshold" )
-            pass
-        elif cmd == "overlay" :
-            robot.config[ "overlay" ] = val
         pass
 
         return "OK"
@@ -353,9 +218,6 @@ if __name__=='__main__':
     if 'stop' in sys.argv :
         stop()
     else :
-        GPIO.setwarnings(False)
-        GPIO.cleanup()
-
         def signal_handler(signal, frame):
             print()
             print( 'You have pressed Ctrl-C.' ) 
@@ -369,6 +231,6 @@ if __name__=='__main__':
 
         signal.signal(signal.SIGINT, signal_handler)
 
-        service()
+        start()
     pass
 pass
